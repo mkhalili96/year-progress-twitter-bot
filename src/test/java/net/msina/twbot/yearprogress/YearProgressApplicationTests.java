c165677bcba19aee8c74761869a7d5a2b9ed3332
mohammad khalili <smkhalili.96@gmail.com>
2020-10-05 20:07:10 +0330

    initial commit

---
+++ src/test/java/net/msina/twbot/yearprogress/YearProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package net.msina.twbot.yearprogress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YearProgressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
